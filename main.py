4994b15fcd4abf92ac7084841c590c069a03b6e5
jalim50 <158749571+jalim50@users.noreply.github.com>
2025-01-09 18:50:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+#hnaya ktbld les information dyalk
 
 gmail="2003011700329@ofppt-edu.ma"
 modpass="Moha2003med@"
 
+##################################################################
 
 
-
-
-
-
+# import psutil
+# for process in psutil.process_iter(attrs=['pid', 'name']):
+#     if process.info['name'] in ('chrome.exe', 'chromedriver.exe'):
+#         process.terminate()  # إنهاء العملية
 
 from selenium import webdriver
 import undetected_chromedriver as uc
@@ -19,11 +21,13 @@ from selenium.webdriver.common.action_chains import ActionChains
 import time
 import random
 
-
-# إعدادات السائق
 def setup_driver():
+    user_data_dir = r"C:\Users\moham\AppData\Local\Google\Chrome\chrome_user_data"
     options = webdriver.ChromeOptions()
-    options.add_argument("--headless=new")
+    options.add_argument(f"--user-data-dir={user_data_dir}")  # استخدام مجلد ملف تعريف المستخدم
+    # options.add_argument("--profile-directory=Default")  # تحديد ملف التعريف الافتراضي
+
+    options.add_argument("--headless=new")           #ila briti Tb9a tchof ach kytra 7Ydi had line
     options.add_argument("--disable-gpu")
     options.add_argument("--window-size=1920,1080")
     options.add_argument("--no-sandbox")
@@ -32,32 +36,25 @@ def setup_driver():
     options.add_argument(
         "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5735.199 Safari/537.36"
     )
+    # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     driver = uc.Chrome(options=options)
     return driver
+
 
 
 # وظيفة لتسجيل الدخول
 def login(driver, email, password):
     try:
         driver.get("https://app.ofppt-langues.ma/gw/api/saml/init?idp=https://sts.windows.net/dae54ad7-43df-47b7-ae86-4ac13ae567af/")
-        time.sleep(1)
         send_key(driver, '//*[@id="i0116"]', email)
-        time.sleep(1)
-        click_element_with_css_selector(driver,'#idSIButton9')
-        time.sleep(1)
+        click_element_with_mouse(driver, '//*[@id="idSIButton9"]')
         send_key(driver, '//*[@id="i0118"]', password)
-        time.sleep(1)
-        click_element_with_css_selector(driver,'#idSIButton9')
-        time.sleep(1)
-        click_element_with_css_selector(driver,'#idSIButton9')
-        time.sleep(1)
-        print('we  login into the account ')
+        click_element_with_mouse(driver, '//*[@id="idSIButton9"]')
+        click_element_with_mouse(driver, '//*[@id="idSIButton9"]')
+        print("we login into the account")
         
     except Exception as e:
-        print('login error')
-        print("Current Time:",  time.strftime("%H:%M:%S", time.localtime()))
         print(f"Error in login: {e}")
-        
         
 
 
@@ -67,7 +64,6 @@ def send_key(driver, xpath, key):
         element = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, xpath)))
         element.send_keys(key)
     except Exception as e:
-        print("Current Time:",  time.strftime("%H:%M:%S", time.localtime()))
         print(f"Error in send_key: {e}")
 
 
@@ -81,20 +77,23 @@ def click_element_with_mouse(driver, xpath):
         actions.click().perform()
         time.sleep(random.uniform(2, 5))
     except Exception as e:
-        print("Current Time:",  time.strftime("%H:%M:%S", time.localtime()))
         print(f"Error in click_element_with_mouse: {e}")
 
 
 def click_element_with_css_selector(driver, css_selector):
     try:
         element = WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.CSS_SELECTOR, css_selector)))
-        element.click()
+        actions = ActionChains(driver)
+        actions.move_to_element(element)
+        time.sleep(random.uniform(2, 5))
+        actions.click().perform()
         time.sleep(random.uniform(2, 5))  # تأخير عشوائي لمحاكاة السلوك البشري
     except Exception as e:
-        print("Current Time:",  time.strftime("%H:%M:%S", time.localtime()))
         print(f"Error in click_element_with_css_selector: {e}")
-        
 
+
+
+# وظيفة لتجاوز الفيديو
 def skip_video(driver):
     try:
         video =  WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.CSS_SELECTOR, '#theme-provider > div.c-bUvWKu > main > div > div > div.c-bQzyIt.c-bQzyIt-kqOPqT-alignContent-start.c-bQzyIt-ddIBXx-gap-4 > div > div > div.plyr__video-wrapper > video')))
@@ -106,7 +105,6 @@ def skip_video(driver):
     except Exception as e:
         print(f"Error in skip_video: {e}")
 
-    
 def wait_video(driver):
     try:
         video =  WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.CSS_SELECTOR, '#theme-provider > div.c-bUvWKu > main > div > div > div.c-bQzyIt.c-bQzyIt-kqOPqT-alignContent-start.c-bQzyIt-ddIBXx-gap-4 > div > div > div.plyr__video-wrapper > video')))
@@ -118,51 +116,90 @@ def wait_video(driver):
         print(f"Error in skip_video: {e}")
 
 
+    
+
+
+def get_all_elements(driver, Selector):
+    try:
+        # انتظار ظهور العنصر الأساسي
+        container = WebDriverWait(driver, 30).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, Selector))
+        )
+        # الحصول على جميع العناصر داخل العنصر الأساسي
+        elements = container.find_elements(By.XPATH, "*")
+        new_elements=[]
+        for element in elements:
+            CenvertSelector = driver.execute_script("""
+                            const el = arguments[0];
+                            const tag = el.tagName.toLowerCase();
+                            const id = el.id ? '#' + el.id : '';
+                            const classes = el.className && typeof el.className === 'string' ? '.' + el.className.replace(/ /g, '.') : '';
+                            return tag + id + classes;
+                        """, element)
+            new_elements.append(CenvertSelector)
+
+
+        selectors=[]
+        for i in range(1,(len(new_elements)+1)):
+            selector=f'#theme-provider > div.c-bUvWKu > main > div > div:nth-child(3) > div.c-gAkLYW > a:nth-child({i})'
+            selectors.append(selector)
+        return selectors
+
+
+     
+    except Exception as e:
+        print('eroor in get_all_elements: ',e)
+
+
+
+
+
 
 
 # الوظيفة الرئيسية
 def main():
-   
     driver = setup_driver()
+    print("welcom to jalim script")
     try:
-        
         login(driver, gmail, modpass)
-        time.sleep(60)
-        driver.get("https://www.google.com")
-        time.sleep(40)
-        n=0
-        driver.get("https://app.ofppt-langues.ma/platform/discover")
+        time.sleep(15)
+        lessons=[]
+        for i in range(1,19):
+            lesson=f'#VOCABULARY > ul > li:nth-child({i})'
+            lessons.append(lesson)
+        
+        
         while True:
+            driver.get('https://app.ofppt-langues.ma/gw/api/saml/init?idp=https://sts.windows.net/dae54ad7-43df-47b7-ae86-4ac13ae567af/')
+            time.sleep(10)
+            driver.get("https://app.ofppt-langues.ma/platform/discover")
+            time.sleep(3)
             # تحقق من أن الرابط الحالي هو الرابط المطلوب
             if driver.current_url == "https://app.ofppt-langues.ma/platform/discover":
                 print("we got the page !")
             else:
-                print(f"we did not get the page the current page is : {driver.current_url}",  time.strftime("%H:%M:%S", time.localtime()))
-                
-                time.sleep(20)
-                driver.get("https://app.ofppt-langues.ma/platform/discover")
-                click_element_with_css_selector(driver,'#theme-provider > div.c-bUvWKu > header > nav > ul > li:nth-child(2) > a')
+                print(f"the current page is: {driver.current_url}")
                 continue
 
-            click_element_with_css_selector(driver,'#VOCABULARY > ul > li:nth-child(1) > a > p')
-            for i in range(1,6):
-                click_element_with_css_selector(driver,f'#theme-provider > div.c-bUvWKu > main > div > div:nth-child(3) > div > a:nth-child({i})')
-                click_element_with_css_selector(driver,'#theme-provider > div.c-bUvWKu > main > div > ul.c-dYOPMy > li:nth-child(1)')
-                click_element_with_css_selector(driver,'#theme-provider > div.c-bUvWKu > main > div > div > div.c-bQzyIt.c-bQzyIt-kqOPqT-alignContent-start.c-bQzyIt-ddIBXx-gap-4 > div > div > button')
-                skip_video(driver)
-                click_element_with_css_selector(driver,'#theme-provider > div.c-bUvWKu > main > div > div.c-UazGY.c-UazGY-hySSfO-gap-12')
-                click_element_with_css_selector(driver,'#theme-provider > div.c-bUvWKu > main > div > ul.c-dXWjRp > li:nth-child(2)')
-            click_element_with_css_selector(driver,'#theme-provider > div.c-bUvWKu > main > div > ul.c-dXWjRp > li:nth-child(1)')
 
-                
-            n+=1
-            print(n)
-            
-            
+            for lesson in lessons:
+                n=0
+                click_element_with_css_selector(driver, lesson)
 
-
+                # التعامل مع الدروس
+                tip_selectors = get_all_elements(driver, '#theme-provider > div.c-bUvWKu > main > div > div:nth-child(3) > div')
+                for tip in tip_selectors:
+                    click_element_with_css_selector(driver, tip)
+                    click_element_with_css_selector(driver, '#theme-provider > div.c-bUvWKu > main > div > ul.c-dYOPMy > li:nth-child(1)')
+                    skip_video(driver)
+                    click_element_with_mouse(driver, '//*[@id="theme-provider"]/div[1]/main/div/div[2]/a')
+                    click_element_with_css_selector(driver,'#theme-provider > div.c-bUvWKu > main > div > ul.c-dXWjRp > li:nth-child(2)')
+                    n+=1
+                    print(n)
+                    if tip==tip_selectors[-1]:
+                        click_element_with_css_selector(driver,'#theme-provider > div.c-bUvWKu > main > div > ul.c-dXWjRp > li:nth-child(1)')
+                        print('done!')
     except Exception as e:
-        print("Current Time:",  time.strftime("%H:%M:%S", time.localtime()))
         print(f"Error in main loop: {e}")
     finally:
         driver.quit()
